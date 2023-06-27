fa2690e6e90cdbff39a4a9d22733086c3cbc25a0
Bobby Habtamu <bobby.habtamu@gmail.com>
2023-06-26 19:40:39 -0700

    Adds Comments

--- week1/script.js
+++ week1/script.js
@@ -1,1 +1,18 @@
+// Defining the list of variables
+let firstName = "Bobby";
+let lastName = "Habtamu";
+let yearsOfStudy = 1;
+let goal = "pursue a career in cloud development";
 
+// Creating the message with the declared variables using the console.log function
+console.log(
+  "My name is " +
+    firstName +
+    " " +
+    lastName +
+    ". I have been at SCC for " +
+    yearsOfStudy +
+    " years and plan to " +
+    goal +
+    " after completing my courses."
+);
