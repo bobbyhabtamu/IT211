1d3477c15952fa0a02f258be56d297376145e688
Bobby Habtamu <bobby.habtamu@gmail.com>
2023-07-18 23:43:02 -0700

    Validates Password

--- week4/script.js
+++ week4/script.js
@@ -1,1 +1,5 @@
-
+function validatePassword(password) {
+    // Check if the password is at least 8 characters long and contains both uppercase and lowercase letters
+    return password.length >= 8 && /[a-z]/.test(password) && /[A-Z]/.test(password);
+  }
+  
