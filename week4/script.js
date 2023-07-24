6987c57ef66b02c01fd964caf92c294a6b2d5766
Bobby Habtamu <bobby.habtamu@gmail.com>
2023-07-23 23:02:56 -0700

    Adds Comments

--- week4/script.js
+++ week4/script.js
@@ -3,3 +3,24 @@ function validatePassword(password) {
     return password.length >= 8 && /[a-z]/.test(password) && /[A-Z]/.test(password);
   }
   
+  function validateForm() {
+    const firstName = document.getElementById('firstName').value.trim();
+    const lastName = document.getElementById('lastName').value.trim();
+    const password = document.getElementById('password').value;
+    const validationMessage = document.getElementById('validationMessage');
+  
+    // Check if first and last name are not missing
+    if (!firstName || !lastName) {
+      validationMessage.textContent = 'Please enter both first and last names.';
+      return;
+    }
+  
+    // Check if the password is valid
+    if (!validatePassword(password)) {
+      validationMessage.textContent = 'Password should have at least 8 characters, at least one uppercase letter, and at least one lowercase letter.';
+      return;
+    }
+  
+    // Validation Message
+    validationMessage.textContent = 'All fields are valid!';
+  }
